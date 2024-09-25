a5a691075cf292e9cfa05670f09e80cb4936866b
edson gabriel <106999196+gabileal@users.noreply.github.com>
2024-09-25 11:32:27 -0300

    25/09

--- InformacoesGlobais.js
+++ InformacoesGlobais.js
@@ -4,6 +4,13 @@ async function vizualizarIformacoesGlobais(params) {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
-    }
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passam em media ${dados.tempo_medio} horas conectas.`
+    console.log(paragrafo)
+    
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
 
     vizualizarIformacoesGlobais() 
