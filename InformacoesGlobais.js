9ec2fce0f4831ab8fc17a5f52e77715a0d1b4a08
edson gabriel <106999196+gabileal@users.noreply.github.com>
2024-10-02 10:46:39 -0300

    2/10

--- InformacoesGlobais.js
+++ InformacoesGlobais.js
@@ -1,16 +1,12 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarIformacoesGlobais(params) {
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados);
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = `voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passam em media ${dados.tempo_medio} horas conectas.`
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+   paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(paragrafo)
-    
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
-}
-
-    vizualizarIformacoesGlobais() 
+  }
+  
+  vizualizarInformacoesGlobais() 
